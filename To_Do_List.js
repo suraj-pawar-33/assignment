e6de230f50e114f23a52283d15d1f495a5e787c8
iamsuraj <psuraj297@gmail.com>
2019-10-28 04:43:54 +0530

    sort function working

--- To_Do_List.js
+++ To_Do_List.js
@@ -9,16 +9,30 @@ function init(){
 
   var add_btn = document.getElementById("add-btn");
   var input_btn = document.getElementById("element");
+  var sort_btn = document.getElementById("sort");
 
   input_btn.addEventListener('keyup', checkEnter);
   add_btn.addEventListener('click', createList);
-
+  sort_btn.addEventListener('click', sort);
 
   function Elements(id, element){
       this.id = id;
       this.element = element;
-      this.pos = 0;
+      this.check = false;
   }
+
+  function sort(ev){
+
+    if(ev.target.sorted == false){
+      sortbyTime();
+      ev.target.sorted = true;
+    }else {
+      sortbyName();
+      ev.target.sorted = false;
+    }
+  }
+
+
 
   function checkEnter(ev){
 
@@ -52,8 +66,10 @@ function init(){
 
   function insertLi(li){
 
-      let pos = getLocation();
-      ul.insertBefore(li, ul.childNodes[pos]);
+      input = input.toLowerCase();
+      data[counter] = new Elements(counter, input);
+
+      ul.insertBefore(li, ul.childNodes[counter]);
 
       input_btn.value = "";
       input_btn.focus();
@@ -61,42 +77,23 @@ function init(){
       counter++;
   }
 
-  function getLocation(){
-      let n = 0;
-      let position = 0;
-      input = input.toLowerCase();
-      data[counter] = new Elements(counter, input);
-
-      data = data.sort((a, b) => {
-          if(a.element > b.element) return 1;
-          else if(a.element < b.element) return -1;
-          else return 0;
-      });
-
-      data.forEach((item) => {
-          item.pos = n;
-          if (item.element == input){position = item.pos;}
-          n++;
-      });
-      console.log(data);
-
-      return position;
-  }
 
   function getNewLi(){
       let li = document.createElement("li");
       li.id = counter;
       li.innerHTML = "<div><p></p></div>";
-      li.addEventListener('click', removeElement)
+      li.addEventListener('click', checkEvent);
       li.appendChild(getDelete());
       li.insertBefore(getCheckBox(), li.getElementsByTagName('div')[0]);
 
       return li;
   }
-  function removeElement(ev){
+  function checkEvent(ev){
       if(ev.target.p == 'delete_btn'){
         ev.currentTarget.remove();
         removeData(ev.currentTarget.id);
+      }else if (ev.target.p == 'check_btn') {
+        togglecheck(ev);
       }
   }
 
@@ -104,17 +101,52 @@ function init(){
       let check = document.createElement("span");
       check.p = 'check_btn';
       check.innerHTML = "";
-      check.addEventListener('click', togglecheck);
+      check.addEventListener('click', nothing);
       return check;
   }
 
   function togglecheck(ev){
 
       if (ev.target.innerHTML == ""){
+          data[ev.currentTarget.id].check = true;
           ev.target.innerHTML = "&#10004";
       }else{
+          data[ev.currentTarget.id].check = false;
           ev.target.innerHTML = "";
       }
+  }
+  function sortbyTime(){
+
+    data.sort((a, b) => {
+        if(a.id > b.id) return 1;
+        else if(a.id < b.id) return -1;
+        else return 0;
+    });
+    changePosition();
+  }
+
+  function sortbyName(){
+    data.sort((a, b) => {
+        if(a.element > b.element) return 1;
+        else if(a.element < b.element) return -1;
+        else return 0;
+    });
+    changePosition();
+  }
+
+  function changePosition(){
+    let i = 0;
+    data.forEach((item) => {
+      let li = document.getElementById(i++);
+      let span = li.getElementsByTagName("span")[0];
+      li.getElementsByTagName("p")[0].innerHTML = item.element;
+      if(item.check == true){
+        span.innerHTML = "&#10004";
+      }else {
+        span.innerHTML = "";
+      }
+    });
+    console.log("After sort",data);
   }
 
   function getDelete(){
@@ -132,8 +164,12 @@ function init(){
       data = data.filter(function(item){
         return (item.id != num);
       });
+      let i = 0;
+      data.forEach((item) => {
+        ul.childNodes[i].id = i;
+        item.id = i++;
+      });
       //to remove the empty cells from the array
-
-      console.log("Now data :",data.length);
+      console.log("after delete :",data);
   }
 }
