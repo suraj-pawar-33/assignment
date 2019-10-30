fb5d5e2b7bbd44cbc3a0bb6550f671c7c90cfa6f
iamsuraj <psuraj297@gmail.com>
2019-10-30 09:00:08 +0530

    add to cart function

--- pizza/pizza.js
+++ pizza/pizza.js
@@ -59,6 +59,10 @@ class Pizza {
     this.imageUrl = obj.imageUrl;
     this.discount = obj.discount;
     this.quantity = 1;
+
+
+    this.h4 = document.createElement("h4");
+    this.inCart = false;
   }
   discPrice() {
     return this.price - ((this.discount / 100 )*this.price);
@@ -90,17 +94,26 @@ class Pizza {
     div2.appendChild(this.createPriceSpan());
     div2.appendChild(this.createQuaSpan());
 
-    let btn = document.createElement('button');
-    btn.classList.add('cart_btn', 'green');
-    btn.innerHTML = "ADD TO CART";
-    btn.addEventListener('click', (ev) => {/*nothing for now*/});
-
     div.classList.add("right");
     div.appendChild(h2);
     div.appendChild(p);
     div.appendChild(div2);
-    div.appendChild(btn);
+    div.appendChild(this.addCartBtn());
     return div;
+  }
+  addCartBtn(){
+    let btn = document.createElement('button');
+    btn.classList.add('cart_btn', 'green');
+    btn.innerHTML = "ADD TO CART";
+    btn.addEventListener('click', (ev) => {
+      if (!this.inCart) {
+        let cartDiv = document.getElementById("cart_inner");
+        cartDiv.appendChild(this.createUl());
+        this.inCart = true;
+      }
+
+    });
+    return btn;
   }
   createCircleDiv(){
     let div = document.createElement('div');
@@ -111,14 +124,117 @@ class Pizza {
   createPriceSpan(){
     let span = document.createElement('span');
     span.classList.add("price");
-    span.innerHTML = this.price;
+    span.innerHTML = "$" + this.price;
     return span;
   }
   createQuaSpan(){
     let span = document.createElement('span');
     span.classList.add("quantity");
-    span.innerHTML = "<span>&lt;</span><h4>"+this.quantity+"</h4><span>&gt;</span>";
+    span.appendChild(this.leftSpan());
+    span.appendChild(this.heading4());
+    span.appendChild(this.rightSpan());
     return span;
+  }
+  leftSpan(){
+    let span = document.createElement('span');
+    span.innerHTML = "&lt;";
+    span.addEventListener('click', (ev) => {
+      if(this.quantity > 1){
+        this.quantity -= 1;
+        this.setQuantity(this.quantity);
+      }
+    });
+    return span;
+  }
+  rightSpan(){
+    let span = document.createElement('span');
+    span.innerHTML = "&gt;";
+    span.addEventListener('click', (ev) => {
+      this.quantity += 1;
+      this.setQuantity(this.quantity);
+    });
+    return span;
+  }
+  heading4(){
+    this.h4.innerHTML = this.quantity;
+    return this.h4;
+  }
+  setQuantity(num){
+    this.h4.innerHTML = this.quantity;
+  }
+
+// For In Cart List Of Pizza
+  createUl(){
+    let ul = document.createElement('ul');
+    ul.classList.add("white_ul");
+    ul.appendChild(this.deleteLi());
+    ul.appendChild(this.imageLi());
+    ul.appendChild(this.nameLi());
+    ul.appendChild(this.priceLi());
+    ul.appendChild(this.discLi());
+    ul.appendChild(this.quantityLi());
+
+    ul.addEventListener('click', (ev) => {
+      if(ev.target.myCode == 45){
+        ev.currentTarget.remove();
+        this.quantity = 1;
+      }
+    });
+    return ul;
+  }
+  deleteLi(){
+    let li = this.getLi();
+    let p = this.getPara();
+    p.classList.add("small_close");
+    p.innerHTML = '&times;';
+    p.myCode = 45;
+    p.addEventListener('click', (ev) => {
+        this.inCart = false;
+    });
+    li.appendChild(p);
+    return li;
+  }
+  imageLi(){
+    let li = this.getLi();
+    let div = this.createCircleDiv();
+    div.classList.remove("circle");
+    li.appendChild(div);
+    return li;
+  }
+  nameLi(){
+    let li = this.getLi();
+    let p = this.getPara();
+    p.innerHTML = this.name;
+    li.appendChild(p);
+    return li;
+  }
+  priceLi(){
+    let li = this.getLi();
+    let p = this.getPara();
+    p.innerHTML = this.price;
+    li.appendChild(p);
+    return li;
+  }
+  discLi(){
+    let li = this.getLi();
+    let p = this.getPara();
+    p.innerHTML = this.discPrice();
+    li.appendChild(p);
+    return li;
+  }
+  quantityLi(){
+    let li = this.getLi();
+    let span = this.createQuaSpan();
+    li.appendChild(span);
+    return li;
+  }
+  getLi(){
+    let li = document.createElement('li');
+    return li;
+  }
+  getPara(){
+    let p = document.createElement('p');
+    return p;
   }
 
 }
