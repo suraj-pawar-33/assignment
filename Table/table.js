8d241e5e911d0be867d092a0e3be222cb7c64620
suraj_pawar_33 <psuraj297@gmail.com>
2019-11-04 23:36:19 +0530

    logic for reading all the content

--- Table/table.js
+++ Table/table.js
@@ -1,26 +1,43 @@
-
+// ReadJSON function
   fetch('https://my-json-server.typicode.com/suraj-pawar-33/tableJson/db')
   .then(response => response.text())
   .then(readThis);
   function readThis(text) {
     let data = JSON.parse(text);
-    createArray(data);
+    readData(data);
   }
 
-
-
-function createArray(file) {
-  let value = 0;
-  for(let main in file){
-
+function readData(item) {
+  if(checkTypeOf(item) == "obj"){
+    readObj(item);
+  }else if(checkTypeOf(item) == "arr"){
+    readArray(item);
   }
-  for (var item of file.data) {
-    for (header in item) {
-      console.log(header,"-",item[header]);
-      if(typeof(item[header]) == 'object'){
+}
 
-      }
-    }
+function readObj(item){
+  for(let value in item){
+    console.log(value,"-", item[value]);
+    readData(item[value]);
   }
+}
+
+function readArray(data){
+  data.forEach(function(value){
+    readData(value);
+  });
 
 }
+
+function checkTypeOf(value) {
+  if (value.length != undefined && typeof value != "string") {
+    return "arr";
+  }else if(value.length == undefined && typeof value != "number"){
+    return "obj";
+  }else if (typeof value == "string") {
+    return "str";
+  }else {
+    return "num";
+  }
+}
+//end
